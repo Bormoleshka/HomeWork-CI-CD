66711a93ee1b54990e4a2c15c388d2c9bcedc1eb
Алексей Бормотов <92627133+Bormoleshka@users.noreply.github.com>
2023-12-22 10:46:35 +0300

    5days

---
+++ src/test/java/org/max/lesson3/home/accuweather/GetWeather5DayTest.java
@@ -0,0 +1,46 @@
+import io.restassured.http.Method;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+import seminar.accuweather.location.Location;
+
+import seminar.accuweather.weather.Weather;
+
+
+
+import java.util.HashMap;
+
+import java.util.List;
+
+import java.util.Map;
+
+
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.lessThan;
+public class ForecastsFiveDaysTest extends AccuweatherAbstractTest {
+
+
+public class GetWeather5DayTest extends AccuweatherAbstractTest {
+   
+  @Test
+
+   void testGetResponse() {
+
+       Weather weather = given().queryParam("apikey", getApiKey()).pathParam("locationKey", 50)
+
+               .when().get(getBaseUrl() + "/forecasts/v1/daily/5day/{locationKey}")
+
+               .then().statusCode(200).time(lessThan(2000L))
+
+               .extract().response().body().as(Weather.class);
+
+       Assertions.assertEquals(5, weather.getDailyForecasts().size());
+
+       System.out.println(weather);
+
+   }
+}
