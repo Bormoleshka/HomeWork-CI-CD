1b93b20123ea3328446c91e272566a95474d8799
Алексей Бормотов <92627133+Bormoleshka@users.noreply.github.com>
2023-12-22 10:49:06 +0300

    15days

---
+++ src/test/java/org/max/lesson3/home/accuweather/GetWeather15DayTest.java
@@ -0,0 +1,45 @@
+import io.restassured.http.Method;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+import seminar.accuweather.location.Location;
+
+import seminar.accuweather.weather.Weather;
+
+
+
+import java.util.HashMap;
+
+import java.util.List;
+
+import java.util.Map;
+
+
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.lessThan;
+
+
+
+public class GetWeather15DayTest extends AccuweatherAbstractTest{
+  @Test
+
+   void testGetResponse15() {
+
+       Weather weather = given().queryParam("apikey", getApiKey()).pathParam("locationKey", 50)
+
+               .when().get(getBaseUrl() + "/forecasts/v1/daily/15day/{locationKey}")
+
+               .then().statusCode(200).time(lessThan(2000L))
+
+               .extract().response().body().as(Weather.class);
+
+       Assertions.assertEquals(5, weather.getDailyForecasts().size());
+
+       System.out.println(weather);
+
+   }
+}
