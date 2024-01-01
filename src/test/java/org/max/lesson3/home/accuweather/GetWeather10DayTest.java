423ecc31106ec4fa2067c5c51728cd70dd3ce1f7
Алексей Бормотов <92627133+Bormoleshka@users.noreply.github.com>
2024-01-01 14:50:12 +0300

    Update GetWeather10DayTest.java
    Добавлены аннотации Allure

--- src/test/java/org/max/lesson3/home/accuweather/GetWeather10DayTest.java
+++ src/test/java/org/max/lesson3/home/accuweather/GetWeather10DayTest.java
@@ -1,5 +1,6 @@
 import io.restassured.http.Method;
-
+import io.qameta.allure.*;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 
 import org.junit.jupiter.api.Test;
@@ -21,11 +22,17 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 
 import static org.hamcrest.Matchers.lessThan;
+@Epic(value = "Тестирование API https://developer.accuweather.com/accuweather-forecast-api/apis")
+@Feature(value = "Домашняя работа 6")
 
 
 public class GetWeather10DayTest extends AccuweatherAbstractTest{
 @Test
-
+@Link("http://dataservice.accuweather.com/forecasts/v1/daily/10day/{locationKey}")
+@Owner("Алексей Бормотов")
+@Severity(SeverityLevel.NORMAL)
+@DisplayName("GetWeather10DayTest")
+@Description("GET Weather 10 Days")
    void testGetResponse() {
 
        Weather weather = given().queryParam("apikey", getApiKey()).pathParam("locationKey", 50)
