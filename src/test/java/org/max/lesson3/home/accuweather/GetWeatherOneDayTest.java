7073188d1de52f522615357b72941e03be863ec0
Алексей Бормотов <92627133+Bormoleshka@users.noreply.github.com>
2024-01-01 14:55:43 +0300

    Update GetWeatherOneDayTest.java

--- src/test/java/org/max/lesson3/home/accuweather/GetWeatherOneDayTest.java
+++ src/test/java/org/max/lesson3/home/accuweather/GetWeatherOneDayTest.java
@@ -1,19 +1,48 @@
-package org.max.lesson3.home.accuweather;
+import io.restassured.http.Method;
+
+import io.qameta.allure.*;
 
 import org.hamcrest.Matchers;
+
 import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
-import org.max.lesson3.seminar.accuweather.AccuweatherAbstractTest;
-import org.max.lesson3.seminar.accuweather.weather.Weather;
-import org.max.lesson3.seminar.spoonacular.ConvertAmountsDto;
+
+import seminar.accuweather.location.Location;
+
+import seminar.accuweather.weather.Weather;
+
+
+
+import java.util.HashMap;
 
 import java.util.List;
 
+import java.util.Map;
+
+
+
 import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.lessThan;
+@Epic(value = "Тестирование API https://developer.accuweather.com/accuweather-forecast-api/apis")
+
+@Feature(value = "Домашняя работа 6")
 
 public class GetWeatherOneDayTest extends AccuweatherAbstractTest {
 
     @Test
+
+
+    @Link("http://dataservice.accuweather.com/forecasts/v1/daily/10day/{locationKey}")
+
+    @Owner("Алексей Бормотов")
+
+    @Severity(SeverityLevel.NORMAL)
+
+    @DisplayName("GetWeatherOneDayTest")
+
+    @Description("GET Weather 1 Day")
     void getWeatherOneDay_shouldReturn() {
 
         Weather response = given()
